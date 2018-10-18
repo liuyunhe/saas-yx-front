d8180fd703f2dd7bbec4698e32c806e33862d01b
chenx <445390630@qq.com>
2018-10-18 15:31:37 +0800

    暂存

--- src/pages/Market/ActTpl/ActTpl.vue
+++ src/pages/Market/ActTpl/ActTpl.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="actTpl-container">
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/market' }">活动管理</el-breadcrumb-item>
+      <el-breadcrumb-item>活动模板</el-breadcrumb-item>
+    </el-breadcrumb>
     <el-card>123</el-card>
   </div>
 </template>
