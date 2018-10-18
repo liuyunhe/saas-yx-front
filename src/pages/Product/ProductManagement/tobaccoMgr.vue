fb8b25b211605494943278c5ba9ad34366eb3ad0
liuyunhe <liuyunhe@saotx.cn>
2018-10-18 11:26:47 +0800

    烟草管理

---
+++ src/pages/Product/ProductManagement/tobaccoMgr.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="tobaccoMgr-container">
+        <el-card>烟草管理</el-card>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+
+  export default {
+    name: "",
+    data() {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    created(){
+      console.log(this.$router)
+    }
+
+  }
+</script>
