dd27c6055db1a49e98f084a174192a50ecd48c82
liuyunhe <liuyunhe@saotx.cn>
2018-10-19 19:00:23 +0800

    供应商管理

---
+++ src/pages/Product/SupplyManagement/EditSupplier.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="edit-supplier-container">
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="供应商名称" prop="name" size="small">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="联系人" prop="contact" size="small">
+        <el-input v-model="ruleForm.contact"></el-input>
+      </el-form-item>
+      <el-form-item label="联系电话" prop="mobile" size="small">
+        <el-input v-model="ruleForm.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="地址" prop="addr" size="small">
+        <el-input v-model="ruleForm.addr"></el-input>
+      </el-form-item>
+      <el-form-item label="备注" prop="note" size="small">
+        <el-input v-model="ruleForm.note" placeholder="请输入备注(主营商品、品牌等)"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .edit-supplier-container{
+    background-color:   #fff;
+    padding: 30px 15px;
+  }
+</style>
+<style>
+  .edit-supplier-container .el-input{
+    width: 300px;
+  }
+</style>
+
+<script>
+  export default {
+    props: ['id'],
+    data() {
+      return {
+        ruleForm: {
+          name: '',
+          contact:'',
+          mobile:'',
+          addr:'',
+          note:''
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入活动名称', trigger: 'blur' },
+          ],
+          contact: [
+            { required: true, message: '请输入联系人', trigger: 'blur' },
+          ],
+          mobile: [
+            { required: true, message: '请输入联系电话', trigger: 'blur' },
+          ],
+          addr: [
+            { required: true, message: '请输入地址', trigger: 'blur' },
+          ],
+
+        }
+      }
+    },
+    created(){
+      this.getSupplierMsg(this.id)
+    },
+    methods:{
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            let params = {
+              addr: this.ruleForm.addr,
+              contact: this.ruleForm.contact,
+              mobile: this.ruleForm.mobile,
+              name: this.ruleForm.name,
+              note: this.ruleForm.note
+            }
+            this.postParams(params)
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      postParams(params){
+        this.$request.post('/api/saotx/supplier/saveOrModify',params,true,res => {
+          if(res.ret == '200000'){
+            this.$message({
+              message: '保存成功！',
+              type: 'success'
+            });
+          }else{
+            this.$message({
+              message:res.message,
+              type: 'warning'
+            })
+          }
+        },err => {
+
+        })
+      },
+      getSupplierMsg(id){
+        this.$request.post('/api/saotx/supplier/detail',{id:id}, true, (res) => {
+          if(res.ret == "200000"){
+            console.log(res.data)
+            this.ruleForm.contact = res.data.contact
+            this.ruleForm.name = res.data.name
+            this.ruleForm.mobile = res.data.mobile
+            this.ruleForm.addr = res.data.addr
+            this.ruleForm.note = res.data.note
+          }
+        })
+      }
+    }
+  }
+</script>
---
+++ src/pages/Product/SupplyManagement/Supply.vue
@@ -0,0 +1,265 @@
+<template>
+    <section class="supply-container clearfix">
+        <!--面包屑-->
+        <el-col :span="24" class="breadcrumb-container">
+            <el-button size="small" type="primary" @click="addSupplier">新增供应商</el-button>
+        </el-col>
+        <!--查询表单-->
+        <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+            <el-form :inline="true" :model="filters" label-width="90px">
+                <el-form-item :size="'small'" label="筛选供应商">
+                  <el-input v-model="filters.keywords" placeholder="请输入内容"></el-input>
+                </el-form-item>
+                <el-form-item :size="'small'" label="联系人">
+                  <el-input v-model="filters.contact" placeholder="请输入内容"></el-input>
+                </el-form-item>
+              <el-form-item :size="'small'" label="联系电话">
+                <el-input v-model="filters.mobile" placeholder="请输入内容"></el-input>
+              </el-form-item>
+                <el-form-item :size="'small'" label="状态">
+                  <el-select
+                      v-model="filters.status"
+                      placeholder="请选择">
+                    <el-option
+                        v-for="item in dataStatusList"
+                        :key="item.code"
+                        :label="item.name"
+                        :value="item.code">
+                    </el-option>
+                  </el-select>
+                </el-form-item>
+                <div></div>
+                <el-form-item class="mr0" :size="'small'">
+                  <el-button type="primary" size="small" @click="commitForm">查询</el-button>
+                  <el-button size="small" class="important" @click="getStatus">重置</el-button>
+                </el-form-item>
+                、
+            </el-form>
+        </el-col>
+        <!--列表-->
+        <el-col class="crm-table-wrap" v-loading="listLoading" :span="24">
+            <el-table
+                :data="supplyList"
+                style="width: 100%">
+                <el-table-column
+                    type="index"
+                    label="序号"
+                    width="100">
+                </el-table-column>
+                <el-table-column
+                    prop="name"
+                    label="供应商名称">
+                </el-table-column>
+                <el-table-column
+                    prop="contact"
+                    label="联系人"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="mobile"
+                    label="联系电话">
+                </el-table-column>
+                <el-table-column
+                    prop="status"
+                    label="状态"
+                    width="100">
+                  <template slot-scope="scope">
+                    <span v-if="scope.row.status == 0">{{ '已停用' }}</span>
+                    <span v-else>{{ '已启用' }}</span>
+                  </template>
+
+                </el-table-column>
+                <el-table-column
+                    label="操作"
+                    width="150"
+                >
+                  <template slot-scope="scope">
+                    <el-button
+                      size="mini"
+                      @click="handleEdit(scope.$index, scope.row)"
+                    >编辑</el-button>
+                    <el-button
+                      size="mini"
+                      type="danger"
+                      @click="handleEdit(scope.$index, scope.row)"
+                    >停用</el-button>
+                  </template>
+                </el-table-column>
+            </el-table>
+        </el-col>
+
+        <!--工具条-->
+        <el-col :span="24" class="footer-toolbar">
+            <el-button type="primary" class="btn-jump-to" @click="jumpTo">跳转</el-button>
+            <el-pagination
+                background
+                layout="total,prev, pager, next,jumper"
+                :current-page="currentPage"
+                @current-change="handleCurrentChange"
+                :page-size="pageSize"
+                :total="total"
+                style="float:right;">
+            </el-pagination>
+        </el-col>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    .supply-container{
+        background-color: #fff;
+        padding: 15px;
+    }
+    .el-dropdown {
+        color: #8A96A0 !important;
+        cursor: pointer;
+    }
+    .el-form-item {
+      margin-bottom: 20px;
+    }
+    .crm-tag {
+        float: left;
+        height: 24px;
+        font-size: 12px;
+        line-height: 22px;
+        border: 1px solid #DCE0E6;
+        border-radius: 2px;
+        color: #5E6D82;
+        margin-top: 8px;
+        margin-right: 20px;
+        .ms {
+            float: left;
+            padding: 0 12px;
+        }
+        .del {
+            float: left;
+            width: 24px;
+            text-align: center;
+            border-left: 1px solid #DCE0E6;
+            cursor: pointer;
+        }
+    }
+</style>
+
+<script>
+  export default {
+    name: "",
+    data() {
+      return {
+        //数据状态列表
+        dataStatusList:[],
+        //载入动画显示
+        listLoading: false,
+        //表单
+        filters: {
+          keywords:'',
+          contact:'',
+          mobile:'',
+          status:''
+        },
+        //table内容
+        supplyList: [],
+        //数据总条数
+        total: 0,
+        //分页器当前选中页码
+        currentPage: 1,
+
+        pageNo: 1,
+        pageSize: 10,
+      }
+    },
+    methods: {
+      addSupplier(){
+        this.$router.push({
+          path:"/product/addSupplier"
+        })
+      },
+      //获取列表
+      getSupplyList() {
+        let params = {
+          contact: this.filters.contact,
+          keywords: this.filters.keywords,
+          mobile: this.filters.mobile,
+          pageNo: this.pageNo,
+          pageSize: this.pageSize,
+          status: this.filters.status
+
+        };
+        this.postSearch(params)
+      },
+
+      //从后台拿取数据状态dataStatus
+      getDataStatus(){
+        this.$request.post('/api/saotx/dim/dataStatus',{}, true, (res) => {
+          if (res.ret == '200000') {
+            this.dataStatusList = res.data
+          }
+        })
+      },
+
+      postSearch(params) {
+        this.listLoading = true;
+        this.$request.post('/api/saotx/supplier/list', params, true, (res) => {
+          if (res.ret == '200000') {
+            this.supplyList = res.data.list
+            this.total = res.data.page.count
+            this.pageSize = res.data.page.pageSize
+            this.pageNo = res.data.page.pageNo
+            this.listLoading = false;
+          }
+        })
+      },
+      //查询按钮
+      commitForm() {
+        this.page = 1
+        this.currentPage = 1
+        this.getSupplyList()
+      },
+      //radio按钮,重置
+      getStatus() {
+        this.filters.keywords = ''
+        this.filters.contact = ''
+        this.filters.mobile = ''
+        this.filters.status = ''
+
+        this.pageNo = 1
+        this.currentPage = 1
+        this.getSupplyList()
+      },
+      //编辑
+      handleEdit(index, row) {
+        this.$router.push({path:'/product/editSupplier?id='+row.id})
+      },
+      //分页器功能
+      handleCurrentChange(val) {
+        this.pageNo = val
+        this.currentPage = val
+        this.getSupplyList()
+      },
+      //跳转按钮功能
+      jumpTo() {
+        this.$emit('current-change')
+      },
+      //超过十个字添加title属性
+      // istitle(value) {
+      //   if (value) {
+      //     if (value.length > 5) {
+      //       return value
+      //     } else {
+      //       return ""
+      //     }
+      //   }
+      // },
+      //新增按钮
+      addTemplate() {
+
+      },
+    },
+    created(){
+     this.getDataStatus()
+
+    },
+    mounted() {
+      this.getSupplyList()
+    }
+  }
+</script>
