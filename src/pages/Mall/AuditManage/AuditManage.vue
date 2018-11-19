b880f746516dc8d7e18ff2a5a077cef919861ec8
liuyunhe <liuyunhe@saotx.cn>
2018-11-19 17:22:33 +0800

    默认审核管理进入显示待审核状态订单

--- src/pages/Mall/AuditManage/AuditManage.vue
+++ src/pages/Mall/AuditManage/AuditManage.vue
@@ -212,7 +212,7 @@
         ],
         filters: {
           //订单审核状态：0-待审核；1-审核通过；2-审核不通过
-          jdOrderStatus :'',
+          jdOrderStatus :'0',
           //开始时间
           startTime:'',
           //结束时间
