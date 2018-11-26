3528d22f0c12d971d3bd0f16624e402e7dd4347c
yangxiaoyan <1325040136@qq.com>
2018-11-26 10:03:03 +0800

    调整老虎机图片

--- src/pages/Mall/ScoreAct/ActList.vue
+++ src/pages/Mall/ScoreAct/ActList.vue
@@ -3,15 +3,14 @@
 		<ul>
 			<li v-for='(item,key) in actList' :key='key'>
 				<div class="con">
-					<img :src="item.gameImg"@click="getDetail(key)" alt="" />
+					<img :src="item.gameImg" @click="getDetail(key)" alt="" />
 					<div class="act-info">
 						<div class="act-left">
 							<p>{{item.gameName}}</p>
 							<p>精彩送不停</p>
 						</div>
 						<div class="act-right">
-							<el-switch
-							  v-model="item.status==1?true:false"@change='toggle(key)'>
+							<el-switch v-model="item.status==1?true:false" @change='toggle(key)'>
 							</el-switch>
 						</div>
 					</div>
@@ -26,66 +25,70 @@
 
 <script>
 	export default {
-		data(){
+		data() {
 			return {
-				state:true,
-				actList:[]
+				state: true,
+				actList: []
 			}
 		},
-		created(){
+		created() {
 			this.init();
 		},
-		methods:{
-			init(){
+		methods: {
+			init() {
 				this.$request.post('/sc/saotx/game/getGames', {}, true, (res) => {
 					if(res.code === 200) {
-						this.actList=res.context;
+						this.actList = res.context;
 					}
 				})
 			},
-			getDetail(index){
-				var that=this;
-				this.$router.push({
-					path:'/mall/score/edit',
-					query:{
-						id:that.actList[index].id
-					}
-				})
-			},
-			toggle(key){
-				var id=this.actList[key].id;
-				if(this.actList[key].status==0){
-					
-					this.$confirm('确定要启用该活动？')
-			       	.then(_ => {			           
-			           this.$set(this.actList[key],'status', 1);
-			           this.$request.post('/sc/saotx/game/changeGameState', {
-			           	id:id,
-			           	status:1
-			           }, true, (res) => {
-							if(res.code === 200) {
-								this.init();
-							}
-						})
-			        })
-			        .catch(_ => {})
-				}else {
-					
-					this.$confirm('确定要禁用该活动？')
-			       	.then(_ => {
-			           this.$set(this.actList[key],'status', 0);
-			           this.$request.post('/sc/saotx/game/changeGameState', {
-			           	id:id,
-			           	status:0
-			           }, true, (res) => {
-							if(res.code === 200) {
-								this.init();
-							}
-						})
-			        })
-			        .catch(_ => {})
+			getDetail(index) {
+				var that = this;
+				if(this.actList[index].type == 1) {
+//					老虎机活动
+					this.$router.push({
+						path: '/mall/score/edit',
+						query: {
+							id: that.actList[index].id
+						}
+					})
 				}
-			    
+
+			},
+			toggle(key) {
+				var id = this.actList[key].id;
+				if(this.actList[key].status == 0) {
+
+					this.$confirm('确定要启用该活动？')
+						.then(_ => {
+							this.$set(this.actList[key], 'status', 1);
+							this.$request.post('/sc/saotx/game/changeGameState', {
+								id: id,
+								status: 1
+							}, true, (res) => {
+								if(res.code === 200) {
+									this.init();
+								}
+							})
+						})
+						.catch(_ => {})
+				} else {
+
+					this.$confirm('确定要禁用该活动？')
+						.then(_ => {
+							this.$set(this.actList[key], 'status', 0);
+							this.$request.post('/sc/saotx/game/changeGameState', {
+								id: id,
+								status: 0
+							}, true, (res) => {
+								if(res.code === 200) {
+									this.init();
+								}
+							})
+						})
+						.catch(_ => {})
+				}
+
 			},
 		}
 	}
@@ -112,18 +115,18 @@
 					width: 100%;
 					height: 100%;
 					border: 1px solid #ccc;
-					overflow:hidden;
+					overflow: hidden;
 					img {
 						width: 100%;
 						height: 180px;
 						object-fit: cover;
 					}
 					.act-left {
-						width:65%;
+						width: 65%;
 						float: left;
 					}
 					.act-right {
-						width:30%;
+						width: 30%;
 						float: right;
 						height: 60px;
 						text-align: center;
@@ -132,21 +135,20 @@
 					p {
 						margin: 0;
 						margin-left: 10px;
-						width:100%;
+						width: 100%;
 						overflow: hidden;
-			            text-overflow: ellipsis;
-			            display: -webkit-box;
-			            -webkit-line-clamp: 1;
-			            /*! autoprefixer: off */
-	    				-webkit-box-orient: vertical;
-	    				/* autoprefixer: on */
+						text-overflow: ellipsis;
+						display: -webkit-box;
+						-webkit-line-clamp: 1;
+						/*! autoprefixer: off */
+						-webkit-box-orient: vertical;
+						/* autoprefixer: on */
 						&:nth-child(1) {
 							font-weight: bold;
 							font-size: 16px;
 							margin: 10px 0 10px 10px;
 						}
 					}
-					
 				}
 			}
 		}
