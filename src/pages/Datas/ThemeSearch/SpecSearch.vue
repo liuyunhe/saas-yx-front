ff9058a721ce30cf8ea17de3cb3a1f712f6b8b2f
liuyunhe <liuyunhe@saotx.cn>
2018-12-01 14:45:43 +0800

    fixed规格查询 重置错误

--- src/pages/Datas/ThemeSearch/SpecSearch.vue
+++ src/pages/Datas/ThemeSearch/SpecSearch.vue
@@ -244,12 +244,19 @@
 				}else {
 					this.startTime=new Date().Format('yyyy-MM-dd');
 				}
+
 				this.type1 = 'day';
+				this.searchShow = 'day'
 				this.date1 = new Date().Format('yyyy-MM-dd');
 				this.month=new Date().Format('yyyy-MM');
 				this.brand = this.brandList[0].name;
 				this.spec = '';
-				this.week=this.weekList[0].weekNo;
+				if(this.weekList.length){
+          this.week=this.weekList[0].weekNo;
+				}
+        this.getBrand();
+        this.specChange()
+
 			},
 			getBrief(){
 				var that = this;
