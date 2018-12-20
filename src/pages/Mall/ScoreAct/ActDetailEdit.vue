a4bc7f31f6ee194041074db49861a53b0383042a
yangxiaoyan <1325040136@qq.com>
2018-12-20 20:17:38 +0800

    dfdf

--- src/pages/Mall/ScoreAct/ActDetailEdit.vue
+++ src/pages/Mall/ScoreAct/ActDetailEdit.vue
@@ -602,14 +602,21 @@ export default {
       if(this.priceList.length<3) return this.$message.warning('请最少选择三种奖品')
       var flag=1;var flag1=1;
       this.priceList.forEach((item)=>{
-      	if(item.quantity=='') return flag=0
-      	if(item.probability=='') return flag1=0;
+      	if(item.quantity==='' || item.quantity<0) {     		
+      		flag=0;
+      		return;
+      	}
+      	if(item.probability==='' || item.probability<0) {
+      		flag1=0;
+      		return;
+      	} 
       })
       if(!flag){
-      	return this.$message.warning('请选择奖品数量');
+      	
+      	return this.$message.warning('请输入奖品数量');
       }
       if(!flag1){
-      	return this.$message.warning('请选择奖品的中奖概率');
+      	return this.$message.warning('请输入奖品的中奖概率');
       }
       if(this.checkAll()) return 1;
       this.addActParams.cssStyle = JSON.stringify(this.configItem);
