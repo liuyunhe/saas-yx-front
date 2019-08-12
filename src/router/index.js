16762bef9496fc6fd23d4e0123bc702a8e4babe4
liuyunhe <liuyunhe@saotx.cn>
2019-08-12 17:13:21 +0800

    2019会员日

--- src/router/index.js
+++ src/router/index.js
@@ -230,6 +230,7 @@ const HeSheng = () => import(/* webpackChunkName: "activity" */ '@/pages/Member/
 const SignMgr = () => import(/* webpackChunkName: "activity" */ '@/pages/Member/2019memberDay/SignMgr')
 const CardCollectingMgr = () => import(/* webpackChunkName: "activity" */ '@/pages/Member/2019memberDay/CardCollectingMgr')
 const QAMgr = () => import(/* webpackChunkName: "activity" */ '@/pages/Member/2019memberDay/QAMgr')
+const PartyMgr = () => import(/* webpackChunkName: "activity" */ '@/pages/Member/2019memberDay/PartyMgr')
 
 // 用户
 const Mgr = () => import(/* webpackChunkName: "customer" */ '@/pages/Customer/Mgr/Mgr')
@@ -482,6 +483,7 @@ export default new Router({
         { path: '/memberday/SignMgr', name: '每日扫码', component: SignMgr },
         { path: '/memberday/CardCollectingMgr', name: '社区卡', component: CardCollectingMgr },
         { path: '/memberday/QAMgr', name: '钻石学堂', component: QAMgr },
+        { path: '/memberday/PartyMgr', name: '钻石Party', component: PartyMgr },
 
 
         // 用户
