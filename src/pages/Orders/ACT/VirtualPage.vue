4f6885d9e5f0449afc835ce5801dc317034e1c51
liuyunhe <liuyunhe@saotx.cn>
2019-09-29 11:06:49 +0800

    订单修改

--- src/pages/Orders/ACT/VirtualPage.vue
+++ src/pages/Orders/ACT/VirtualPage.vue
@@ -54,6 +54,7 @@
                 </el-form-item>
             </el-form>
             <el-table :data="listData" style="width: 100%" border>
+                <el-table-column prop="hdCode" label="活动编号" ></el-table-column>
                 <el-table-column prop="orderCode" label="订单号" ></el-table-column>
                 <el-table-column prop="nickname" label="中奖人"></el-table-column>
                 <el-table-column prop="actName" label="活动名称"></el-table-column>
