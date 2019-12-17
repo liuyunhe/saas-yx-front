3ab503f7ae262adc25ef7f7c6002c4b331bf9ad3
liuyunhe <liuyunhe666@aliyun.com>
2019-12-17 10:39:30 +0800

    会员日二期

--- src/pages/Member/2019memberDay/PartyMgr.vue
+++ src/pages/Member/2019memberDay/PartyMgr.vue
@@ -21,14 +21,25 @@
             </el-form-item>
         </el-card>
         <div style="height: 30px"></div>
-        <el-card :body-style="{ padding: '20px' }">
-          <div slot="header" class="clearfix">
-            <span>商品设置：</span>
+
+      </el-form>
+      <el-card :body-style="{ padding: '20px' }">
+        <div slot="header" class="clearfix">
+          <span>商品设置：</span>
+        </div>
+        <div>
+          <el-button size="small" type="primary" @click="addItem" v-if="awardConf.length<5 && !isStart">新增商品</el-button>
+        </div>
+        <template v-for="(ai,key) in awardConf">
+          <div  style="height: 30px;font-size: 18px;line-height: 30px;margin:20px;color: #409EFF">
+            奖项{{ key + 1 }}:
+            <el-button size="mini" type="danger" style="margin-left: 450px" @click="delItem(key)" v-if="key>0 && !isStart">删除奖品</el-button>
           </div>
-          <template v-for="(ai,key) in awardConf" >
-            <div  style="height: 30px;font-size: 18px;line-height: 30px;margin: 30px 0;color: #409EFF">奖项{{ key + 1 }}:</div>
-            <el-form-item label="奖品类型:">
-              <el-select :disabled="!!ai.id" @change="handleSelectChange(ai)" v-model="ai.awardType"  placeholder="请选择">
+
+          <el-form ref="awardConf" :model="ai" label-width="150px" :rules="confRules" :disabled="isStart">
+
+            <el-form-item label="奖品类型：" prop="awardType">
+              <el-select :disabled="!!ai.id" @change="handleSelectChange(ai)" v-model="ai.awardType" style="width: 300px" placeholder="请选择">
                 <el-option
                     v-for="i in awardType"
                     :key="i.value"
@@ -37,51 +48,53 @@
                 </el-option>
               </el-select>
             </el-form-item>
-            <el-form-item v-if="ai.awardType&&!ai.id">
-              <div>选择{{ ai.awardType == '1' ? '实物' : ai.awardType == '3' ? '红包' : '荷石币' }}:
-                <el-button size="" style="margin-left: 20px"  @click="getList(ai.awardType,key)">选择</el-button>
-              </div>
+
+            <el-form-item v-if="ai.awardType&&!ai.id" :label="`选择${ ai.awardType == '1' ? '实物' : ai.awardType == '7' ? '折扣卡' : '翻倍卡' }：`">
+              <el-button size="" @click="getList(ai.awardType,key)">选择</el-button>
             </el-form-item>
-            <el-form-item v-if="ai.awardType&&ai.awardName">
-              <span v-if="ai.awardType !== 1 " style="margin-right: 20px">名称: {{ ai.awardName }}</span>
-              {{ ai.awardType == 3 || ai.awardType == 6 ? "面额:" : "名称:" }}
-              <el-input-number v-if="ai.awardType == 3 || ai.awardType == 6" v-model="ai.awardPrice" :disabled="!!ai.id ? true : false" :precision="2" :min="0" controls-position="right"></el-input-number>
-              {{ ai.awardType == 1 ? ai.awardName : ai.awardType == 3 ? "元" : "荷石币" }}
-              <span style="margin-right: 20px"></span>
 
-              投放数量<el-input-number v-model="ai.totalNum" :disabled="ai.id ? true : false" :precision="0" :min="0" controls-position="right"></el-input-number>个
-              <span v-if="ai.id ? true : false">
-                <span style="margin-right: 20px"></span>
-                剩余{{ ai.totalNum - ai.outNum }}个
-              </span>
-              <span style="margin-right: 20px"></span>
-
-
+            <el-form-item label="物料名称：" prop="awardName">
+              <el-input style="width: 300px" disabled v-model="ai.awardName"></el-input>
             </el-form-item>
-          </template>
-          <div style="margin-bottom: 20px">选择实物:<el-button size="" style="margin-left: 20px"  @click="getList(1)">选择</el-button></div>
-            <el-form-item label='物料名称：' prop="awardName">
-              <el-input style="width: 400px" disabled v-model="config.awardName"></el-input>
-            </el-form-item>
-            <el-form-item label='商品图片：' prop="awardPic">
-              <img width="355" height="289" v-if="config.awardPic" :src="config.awardPic" alt="">
 
-              <el-upload :action="uploadURL" :headers="headerObj" :on-success="upClockImgUrlSuccess" :show-file-list="false">
+            <el-form-item label='奖品图片：' prop="awardPic">
+              <el-input v-model="ai.awardPic" style="display: none" ></el-input>
+              <img width="300" height="165" v-if="ai.awardPic" :src="ai.awardPic" alt="">
+              <el-upload :action="uploadURL" :headers="headerObj" :on-success="(res)=>{uploadImgUrlSuccess(res,key)}" :show-file-list="false">
                 <el-button size="small" type="primary">上传图片</el-button>
                 <span style="margin-left: 20px">上传商品图片尺寸（630x348px）</span>
               </el-upload>
             </el-form-item>
-            <el-form-item label='商品描述：' prop="awardDesc">
-              <el-input type="textarea" style="width: 400px"  v-model="config.awardDesc"></el-input>
+
+            <el-form-item v-if="ai.awardType==7" label='折扣值：' prop="cardValue">
+              <el-input-number :step="0.01" :precision="2":min="0" :max="1" controls-position="right" v-model="ai.cardValue"></el-input-number>
+              <span>折兑换折扣卡</span>
             </el-form-item>
-        </el-card>
-      </el-form>
-      <div style="height: 30px"></div>
+
+            <el-form-item v-if="ai.awardType==8"  label='翻倍值：' prop="cardValue">
+              <el-input-number :step="0.01" :precision="2" :min="1" controls-position="right" v-model="ai.cardValue"></el-input-number>
+              <span>倍红包翻倍卡</span>
+            </el-form-item>
+
+            <el-form-item label='奖品描述：' prop="awardDesc">
+              <el-input type="textarea" style="width: 300px"  v-model="ai.awardDesc"></el-input>
+            </el-form-item>
+
+            <el-form-item label='中奖人数：' prop="prizeNum">
+              <el-input style="width: 300px" type="number"   v-model="ai.prizeNum"></el-input>
+            </el-form-item>
+            <div style="height: 30px"></div>
+          </el-form>
+
+        </template>
+        <div style="margin-bottom: 50px"></div>
+      </el-card>
+
     </div>
 
 
     <div style="height: 40px;text-align: center;margin-top: 30px">
-      <el-button type="primary" @click="confirmSubmit">保存</el-button>
+      <el-button type="primary" v-if="!isStart" @click="confirmSubmit">保存</el-button>
     </div>
 
     <!-- 通用 -->
@@ -112,6 +125,15 @@
 </template>
 
 <script>
+  const awardDemo = {
+      "awardName": "",
+      "marketMoney": null,
+      "cardValue": null,
+      "awardPic": "",
+      "awardDesc": "",
+      "awardType": null,
+      "prizeNum": null
+    }
   export default {
     name: "PartyMgr",
     data(){
@@ -132,27 +154,7 @@
           callback()
         }
       }
-      var validateAwardName = (rule, value, callback) => {
-        if (!this.config.awardName) {
-          callback(new Error('请选择物料'))
-        } else {
-          callback()
-        }
-      }
-      var validateAwardPic = (rule, value, callback) => {
-        if (!this.config.awardPic) {
-          callback(new Error('请上传商品图片'))
-        } else {
-          callback()
-        }
-      }
-      var validateAwardDesc = (rule, value, callback) => {
-        if (!this.config.awardDesc) {
-          callback(new Error('请输入活动说明'))
-        } else {
-          callback()
-        }
-      }
+
       return {
         uploadURL: '/api/wiseqr/attach/commonAliUpload',
         headerObj: {
@@ -161,20 +163,26 @@
           CLIENTSESSIONID: sessionStorage.getItem('CLIENTSESSIONID')
         },
 
+        id: null,
+        isStart:false,
         newAct: false,
         config:{
           stime:'',
           etime:'',
           scanCont:'',
-          awardName:'',
-          marketMoney:'',
-          awardPic:'',
-          awardDesc:''
         },
 
-        awardConf:[{"awardType":6,"awardName":"456","awardPrice":1,"awardPic":"https://qoss.qrmkt.cn/saas_platform/test/DC7DE1D112BD4873816ECCC62CDC1A03.png","totalNum":50,"outNum":0,"status":1}],
-
-
+        awardConf:[
+          {
+            "awardName": "",
+            "marketMoney": null,
+            "cardValue": null,
+            "awardPic": "",
+            "awardDesc": "",
+            "awardType": null,
+            "prizeNum": null
+          }
+        ],
         title: '选择物品',
         list: [],
         params: {
@@ -190,9 +198,12 @@
         confRules: {
           date: [{ required: true, validator: validateDate, trigger: 'change' }],
           scanCont: [{ required: true, validator: validateScanCont, trigger: 'change' }],
-          awardName: [{ required: true, validator: validateAwardName, trigger: 'change' }],
-          awardPic: [{ required: true, validator: validateAwardPic }],
-          awardDesc: [{ required: true, validator: validateAwardDesc,trigger:'blur' }],
+          awardType: [{ required: true, message: '请选择奖品类型', trigger: 'change' }],
+          awardName: [{ required: true, message: '请选择物料', trigger: 'change' }],
+          awardPic: [{ required: true, message: '请上传奖品图片' ,trigger: 'change'}],
+          awardDesc: [{ required: true, message: '请输入奖品描述',trigger:'blur' }],
+          prizeNum: [{ required: true, message: '请输入中奖人数',trigger:'blur' }],
+          cardValue: [{ required: true, message: '请输入面额',trigger:'blur' }],
         },
 
         awardType:[
@@ -201,21 +212,14 @@
             label:'实物'
           },
           {
-            value: 3,
-            label:'红包'
+            value: 7,
+            label:'折扣卡'
           },
           {
-            value: 6,
-            label:'荷石币'
+            value: 8,
+            label:'翻倍卡'
           },
         ],
-        awardDemo:{
-          "awardType": null,
-          "awardName": "",
-          "awardPrice": null,
-          "awardPic": "",
-          "totalNum": null,
-        },
         awardNo:'',
       }
     },
@@ -225,12 +229,38 @@
     mounted() {
     },
     methods:{
+      addItem(){
+        this.awardConf.push(awardDemo)
+      },
+      delItem(index){
+        this.awardConf.splice(index,1)
+      },
       // 当前正在进行的活动查询
       getDetail() {
         this.$request.post('/hbact/one/points/sass/act/config', {}, true, res => {
           if (res.code == '200') {
-            this.config = res.data
+            this.config.stime = res.data.conf.stime
+            this.config.etime = res.data.conf.etime
+            this.config.scanCont = res.data.conf.scanCont
             this.newAct = false
+            this.id = res.data.conf.id
+            this.isStart = res.data.isStart
+            if(res.data.prizeList.length > 0){
+              this.awardConf.splice(0,1)
+              res.data.prizeList.forEach(item => {
+                this.awardConf.push(
+                  {
+                    "awardName": item.awardName,
+                    "marketMoney": item.marketMoney,
+                    "cardValue": item.cardValue,
+                    "awardPic": item.awardPic,
+                    "awardDesc": item.awardDesc,
+                    "awardType": item.awardType,
+                    "prizeNum": item.prizeNum
+                  }
+                )
+              })
+            }
             return
           }else if(res.code == '500'){
             this.newAct = true
@@ -239,31 +269,6 @@
           }
         })
       },
-      // getList(type) {
-      //   if (type == '1') {
-      //     this.params.metraFlag = 'object'
-      //     this.title = '选择实物'
-      //   } else if (type == '2') {
-      //     this.params.metraFlag = 'virtual'
-      //     this.title = '选择虚拟'
-      //   } else if (type == '3') {
-      //     this.params.metraFlag = 'redpack'
-      //     this.title = '选择红包'
-      //   } else if (type == '6') {
-      //     this.params.metraFlag = 'integral'
-      //     this.title = '选择荷石币'
-      //   }
-      //   this.$request.post('/api/wiseqr/metra/list', this.params, true, res => {
-      //     if (res.ret === '200000') {
-      //       this.list = []
-      //       this.list = res.data.list
-      //       this.listTotal = res.data.page.count
-      //       this.listVisible = true
-      //       return
-      //     }
-      //     this.$message.error(res.message)
-      //   })
-      // },
       handleOpen(){
         this.$request.post('/hbact/one/points/sass/open/award', {}, true, res => {
           if (res.code == '200') {
@@ -274,19 +279,28 @@
         })
       },
       confirmSubmit() {
+        let awardConfig = true
+        this.$refs.awardConf.forEach(item => {
+          item.validate(valid => {
+            if(valid){
+
+            }else {
+              awardConfig = false
+            }
+          })
+        })
         this.$refs.actSetConfRef.validate(valid => {
           if(valid){
+            if (!awardConfig) return
             let params = {
-              stime : this.config.stime,
-              etime : this.config.etime,
-              scanCont : this.config.scanCont,
-              awardName : this.config.awardName,
-              marketMoney : this.config.marketMoney,
-              awardPic : this.config.awardPic,
-              awardDesc : this.config.awardDesc,
-              id : this.config.id ? this.config.id : ""
+              conf:this.config,
+              prizeList:this.awardConf
             };
-            this.$request.post('/hbact/one/points/sass/act/add', params, false, res => {
+            console.log(params)
+            if(this.id){
+              params['conf'].id = this.id
+            }
+            this.$request.post('/hbact/one/points/sass/act/save', params, true, res => {
               if (res.code == '200') {
                 this.$message({type: 'success', message: '操作成功!'});
                 this.getDetail()
@@ -307,7 +321,7 @@
         console.log(awardNo)
         this.awardConf[awardNo].awardName = obj.name
         this.awardConf[awardNo].awardPrice = obj.marketMoney
-        this.awardConf[awardNo].awardPic = obj.pic
+        this.awardConf[awardNo].marketMoney = obj.marketMoney
         this.handleColseList()
       },
       handleColseList(){
@@ -322,9 +336,9 @@
         this.params.pageNo = newSize
         this.getList()
       },
-      upClockImgUrlSuccess(resule) {
+      uploadImgUrlSuccess(resule,index) {
         if (resule.ret === '200000')
-          return (this.config.awardPic = resule.data.accessUrl)
+          return (this.awardConf[index].awardPic = resule.data.accessUrl)
         this.$message.error(resule.message)
       },
 
@@ -333,7 +347,6 @@
         console.log(item)
         item.awardName = ""
         item.awardPic = ""
-        item.totalNum = null
       },
       getList(type,key) {
         if (type == '1') {
@@ -351,6 +364,9 @@
         } else if (type == '7') {
           this.params.metraFlag = 'cdDisc'
           this.title = '选择折扣卡'
+        } else if (type == '8') {
+          this.params.metraFlag = 'cdDouble'
+          this.title = '选择翻倍卡'
         }
 
         if(key != -1){
