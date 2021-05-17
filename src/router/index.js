5c408520b70d82dbac6c26204d849ae50b8239ae
liuyunhe <liuyunhe666@aliyun.com>
2021-05-17 16:36:03 +0800

    add 石头剪刀布模板

--- src/router/index.js
+++ src/router/index.js
@@ -55,6 +55,7 @@ const AddCapsuleToys = () => import(/* webpackChunkName: "activity" */ '@/pages/
 const AddDadishu = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddDadishu')
 const AddDaqiqiu = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddDaqiqiu')
 const AddJiawawa = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddJiawawa')
+const AddRockPaperScissors = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddRockPaperScissors')
 const AddDiaohongbao = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddDiaohongbao')
 const AddHePiaoXiang = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddHePiaoXiang')
 const AddWingAct = () => import(/* webpackChunkName: "activity" */ '@/pages/Market/ActTpl/AddWingAct')
@@ -429,6 +430,7 @@ export default new Router({
         { path: '/market/actTpl/addDadishu', name: '新建打地鼠活动模板配置', component: AddDadishu, props: (router) => ({id: router.query.id, edit: router.query.edit})},
         { path: '/market/actTpl/addDaqiqiu', name: '新建打气球活动模板配置', component: AddDaqiqiu, props: (router) => ({id: router.query.id, edit: router.query.edit})},
         { path: '/market/actTpl/addJiawawa', name: '新建夹娃娃活动模板配置', component: AddJiawawa, props: (router) => ({id: router.query.id, edit: router.query.edit})},
+        { path: '/market/actTpl/addRockPaperScissors', name: '新建夹娃娃活动模板配置', component: AddRockPaperScissors, props: (router) => ({id: router.query.id, edit: router.query.edit})},
         { path: '/market/actTpl/addDiaohongbao', name: '新建钓红包活动模板配置', component: AddDiaohongbao, props: (router) => ({id: router.query.id, edit: router.query.edit})},
         { path: '/market/actTpl/addHePiaoXiang', name: '新建荷飘香活动模板配置', component: AddHePiaoXiang, props: (router) => ({id: router.query.id, edit: router.query.edit})},
         { path: '/market/actTpl/addActFanpaizi', name: '翻牌子活动', component: Fanpaizi, props: (router) => ({id: router.query.id, edit: router.query.edit})},
